a249215ad92c5615b7b7bf30c53788121fc3de6e
joe wang <wangmrjoe@gmail.com>
2018-12-18 00:44:46 +0800

    mm

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<button :class = "num == i? 'active' : ''" @click="change(i)" v-for="(o,i) in btlist">{{o}}-->
+
+      <!--<div :class ="num == i ? 'active' : ''">我是{{o}}</div>-->
+    <!--</button>-->
+
+    <HelloWorld msg="Welcome to Your Vue.js App" sayhi="hello joe"/>
   </div>
 </template>
 
@@ -11,8 +15,21 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+      HelloWorld
+  },
+    data(){
+      return{
+          btlist:['新闻','衣服','柜子'],
+					num:1,
+
+      }
+    },
+    methods:{
+        change(i){
+            this.num = i;
+
+        }
+    }
 }
 </script>
 
@@ -25,4 +42,23 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+#app button.active{
+  background-color: aquamarine;
+  position: relative;
+}
+
+
+  #app button div{
+    width: 200px;
+    height: 200px;
+    border: 1px solid #ccc;
+    position: absolute;
+    display: none;
+  }
+
+#app button div.active{
+    display: block;
+}
+
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,74 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <!--<h1>{{msg}}</h1>-->
+
+        <input type="checkbox" :checked="qx" @click="chbox(qx)">全选
+        <hr>
+        <input :checked="ccbox" :value='ccbox' @click="inbox(ccbox,index)"  v-for="(n) in index" :key="n.index"  type="checkbox">
+
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        data() {
+            return {
+                index:[1,2,3,4,5],
+                qx:'',
+                ccbox:'',
+                nn:1,
+            }
+        },
+        methods: {
+            chbox(qx){
+                this.qx =!qx;
+                this.ccbox = !qx;
+                if(this.qx == true){
+                    this.nn=this.index.length;
+                }else{
+                    this.nn=1;
+                }
+            },
+
+            inbox(cc,n){
+
+                if(cc){
+                    this.nn++
+                }else {
+                    this.nn--;
+                }
+                if(this.nn==n.length){
+                    this.qx=true;
+                }else {
+                    this.qx=false;
+                }
+            }
+        }
+
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
